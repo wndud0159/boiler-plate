d4f9164286481a977810d44f13c0117901c8fb11
wndud0159 <wndud0159@naver.com>
2021-10-08 23:03:01 +0900

    push step2

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+const { User } = require('./models/User')
+
+const config = require('./config/key')
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+
 
 // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://juyoung:qwer1234@cluster0.xplpu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
     .then(() => console.log('MongoDB Connection...'))
     .catch(err => console.log(err))
 
 
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.send('hello world juyoung!!'))
+
+
+app.post('/register', (req, res) => {
+    // 회원가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
